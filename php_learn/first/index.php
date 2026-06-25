0b53dd11d5abb3cf4ccb17e0600a8e0196658139
Saeed Gholami <saeed.ghollami@gmail.com>
2026-06-25 20:00:03 +0330

    some php stuff

--- php_learn/first/index.php
+++ php_learn/first/index.php
@@ -1,16 +1,17 @@
 <html>
+
 <head>
     <title>First php page</title>
 </head>
+
 <body>
-    <h1>First time using php</h1>
+    <h1>Calculate two integers</h1>
+
     <?php
-        $name = "Saeed Gholami";
-        echo "<p>Hello, $name</p>";
+    $name = "Saeed Gholami";
+    echo "<p>Hello, $name</p>";
     ?>
     <p>After running some page</p>
 </body>
+
 </html>
-
-
-
